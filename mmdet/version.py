65d14d68819011ba27b420807f0f1ed16e1c7646
EuphoriaYan <ysq58000@foxmail.com>
2020-11-06 00:58:20 +0800

    update

--- mmdet/version.py
+++ mmdet/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Fri Oct 30 23:49:22 2020
+# TIME: Fri Nov  6 00:55:51 2020
 
-__version__ = '1.0rc1+4357697'
-short_version = '1.0rc1'
+__version__ = '1.0.rc0+ce63051'
+short_version = '1.0.rc0'
